6fcbdd90dc356b5231ea4dc2ebd4ae323378ef88
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-06-01 16:45:30 -0400

    Update gm.h

--- gm.h
+++ gm.h
@@ -1109,7 +1109,7 @@
         }
 
         if (collision_info) {
-            collision_info->normal = gm_vec2_normalize(gm_vec2_sub(c2.position, c1.position));
+            collision_info->normal = gm_vec2_normalize(gm_vec2_sub(c1.position, c2.position));
             collision_info->depth = total_radius - distance;
         }
 
@@ -1197,11 +1197,16 @@
                 if (object_a->collider.type == GM_COLLIDER_CIRCLE && object_b->collider.type == GM_COLLIDER_CIRCLE) {
                     GM_CollisionInfo2D collision_info = {0};
                     if (gm_collision2d_circles(object_a->collider.circle, object_b->collider.circle, &collision_info)) {
-                        object_a->rb.position = gm_vec2_add(object_a->rb.position, gm_vec2_scale(collision_info.normal, -collision_info.depth / 2.0f));
-                        object_a->collider.circle.position = object_a->rb.position;
+                        if (collision_info.depth / object_a->collider.circle.radius >= 0.75) {
+                            object_a->rb.position = gm_vec2_add(object_a->rb.position, gm_vec2_scale(collision_info.normal, collision_info.depth / 2.0f));
+                            object_a->collider.circle.position = object_a->rb.position;
 
-                        object_b->rb.position = gm_vec2_add(object_b->rb.position, gm_vec2_scale(collision_info.normal, collision_info.depth / 2.0f));
-                        object_b->collider.circle.position = object_b->rb.position;
+                            object_b->rb.position = gm_vec2_add(object_b->rb.position, gm_vec2_scale(collision_info.normal, -collision_info.depth / 2.0f));
+                            object_b->collider.circle.position = object_b->rb.position;
+                        } else {
+                            object_a->rb.velocity = gm_vec2_add(object_a->rb.velocity, gm_vec2_scale(collision_info.normal, collision_info.depth / 2.0f));
+                            object_b->rb.velocity = gm_vec2_add(object_b->rb.velocity, gm_vec2_scale(collision_info.normal, -collision_info.depth / 2.0f));
+                        }
                     }
                 }
             }
