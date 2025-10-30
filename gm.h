af854811150a990a87bbeddedd2d897bcdbcb0fd
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-10-30 09:38:09 -0400

    fixing weird assignment usage

--- gm.h
+++ gm.h
@@ -1829,11 +1829,14 @@
         if (t < 1.0f / d1) {
             return n1 * t * t;
         } else if (t < 2.0f / d1) {
-            return n1 * (t -= 1.5f / d1) * t + 0.75f;
+            t -= 1.5f;
+            return n1 * (t / d1) * t + 0.75f;
         } else if (t < 2.5f / d1) {
-            return n1 * (t -= 2.25f / d1) * t + 0.9375f;
+            t -= 2.25f;
+            return n1 * (t / d1) * t + 0.9375f;
         } else {
-            return n1 * (t -= 2.625f / d1) * t + 0.984375f;
+            t -= 2.625f;
+            return n1 * (t / d1) * t + 0.984375f;
         }
     }
 
